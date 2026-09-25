d1379009eb895aa2353e08930e75b551c70fe495
alinstan1 <alinwillwide@gmail.com>
2026-09-25 23:06:20 +0300

    fix: update import paths for consistency in component references

--- src/dynamic/index.ts
+++ src/dynamic/index.ts
@@ -1,13 +1,13 @@
 import type { Component } from "svelte";
 
 export type ImportPath =
-    | "./Toc.svelte"
-    | "./prompf/Pronunciation.svelte"
+    | "./prompf/Toc.svelte"
+    | "./prompf/pronunciation/Pronunciation.svelte"
     | "./timeline/Menu.svelte"
     ;
 
 export const i: Map<ImportPath, Promise<{ default: Component<any> }>> = new Map();
 
-i.set("./Toc.svelte", import("../prompf/Toc.svelte"));
-i.set("./prompf/Pronunciation.svelte", import("../prompf/pronunciation/Pronunciation.svelte"));
+i.set("./prompf/Toc.svelte", import("../prompf/Toc.svelte"));
+i.set("./prompf/pronunciation/Pronunciation.svelte", import("../prompf/pronunciation/Pronunciation.svelte"));
 i.set("./timeline/Menu.svelte", import("../timeline/Menu.svelte"));
